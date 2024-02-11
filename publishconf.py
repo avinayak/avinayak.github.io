31a1df2862c3833e14aadc5fc7a7636b30ff4665
Atul Vinayak <su.atul.vi@gmail.com>
2024-02-11 00:20:44 -0800

    asd

--- publishconf.py
+++ publishconf.py
@@ -14,7 +14,7 @@ RELATIVE_URLS = False
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
-DELETE_OUTPUT_DIRECTORY = True
+# DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 PELICAN_CONTENT_FOLDER = "content"
